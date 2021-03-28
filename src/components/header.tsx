cf7f984aa4cc50d23843ad830ae3c15946570cd4
nazna <14846330+nazna@users.noreply.github.com>
2021-03-28 18:53:32 +0900

    chore: fix top page design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
+import clsx from 'clsx'
 import styles from '../styles/index.module.css'
 
 export function Header() {
   return (
-    <header className={styles.container}>
+    <header className={clsx(styles.header, styles.container)}>
       <a className={styles.avatar} href="/">
-        <img width="48px" height="48px" src="/avatar.jpg" />
+        <img width="48px" height="48px" src="avatar.png" alt="nazna's avatar" />
       </a>
     </header>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,14 @@ export default class Document extends NextDocument {
     return (
       <Html lang="ja">
         <Head>
+          <meta name="theme-color" content="#0e8b9f" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content="nazna.dev" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400:600&display=swap" rel="stylesheet" />
+          <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+          <link rel="manifest" href="nazna.dev.webmanifest" />
+          <link rel="preconnect" href="//fonts.gstatic.com" />
+          <link rel="stylesheet" href="//fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" />
+          <title>nazna.dev</title>
         </Head>
         <body>
           <noscript>You need to enable JavaScript to run this app.</noscript>
